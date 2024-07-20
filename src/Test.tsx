303d3a051eb585a869f4010a9e2d90b0a0a46447
Ellan <ellan@Ellan-Gichuru.local>
2024-07-20 21:25:10 +0300

    ellans mac was not the problem confirmation code

--- src/Test.tsx
+++ src/Test.tsx
@@ -1,24 +1,41 @@
 const Test = () => {
   return (
-    // <section className="parallax border border-red-500">
-    //   <div className="grid grid-col">
-    //     <p className="border border-green-500 m-2">Testing This</p>
-    //     <div className="flex justify-center border border-blue-500 m-2">
-    //       <img src="/AWSLogo.png" alt="Example" />
-    //       <p>Hello There</p>
-    //     </div>
-    //   </div>
-    // </section>
-    <div className="border border-white min-h-screen grid grid-rows-2">
-      <div className="flex items-center justify-center border border-red-500 text-center">
+    <div className="border border-red-500 min-h-screen flex flex-col">
+      <div className="w-[300px] flex-1 flex items-center justify-center border border-white mb-6">
         AWS Community Kenya
       </div>
-      <div className="p-4 flex flex-row">
-        <div className="w-[300px] border border-green-500 flex items-center justify-center">
-          Card1
-        </div>
-        <div className="w-[300px] border border-blue-500 flex items-center justify-center">
-          Card2
+      <div className="flex-1 flex items-center justify-center">
+        <div className="flex w-full max-w-4xl">
+          <div className="p-4 flex-1 border border-green-500 m-2 flex items-center justify-center">
+            AWS Community Day is a dynamic event celebrating the AWS community,
+            uniting cloud enthusiasts, developers, and professionals from
+            diverse fields. This event highlights the expansive universe of AWS
+            technologies, offering participants the chance to dive into
+            educational sessions, engage in practical workshops, and expand
+            their professional networks. Attendees will explore cutting-edge
+            trends and practical applications of AWS services, fostering a
+            collaborative environment rich in knowledge exchange and innovation.
+            The event is designed to provide a platform for learning and
+            sharing, with opportunities to gain insights from AWS experts and
+            industry leaders. Join us to connect with peers, enhance your AWS
+            skills, and become part of a thriving community driven by shared
+            learning and growth.
+          </div>
+          <div className="flex-1 border border-blue-500 m-2 flex items-center justify-center">
+            AWS Community Day is a dynamic event celebrating the AWS community,
+            uniting cloud enthusiasts, developers, and professionals from
+            diverse fields. This event highlights the expansive universe of AWS
+            technologies, offering participants the chance to dive into
+            educational sessions, engage in practical workshops, and expand
+            their professional networks. Attendees will explore cutting-edge
+            trends and practical applications of AWS services, fostering a
+            collaborative environment rich in knowledge exchange and innovation.
+            The event is designed to provide a platform for learning and
+            sharing, with opportunities to gain insights from AWS experts and
+            industry leaders. Join us to connect with peers, enhance your AWS
+            skills, and become part of a thriving community driven by shared
+            learning and growth.
+          </div>
         </div>
       </div>
     </div>
