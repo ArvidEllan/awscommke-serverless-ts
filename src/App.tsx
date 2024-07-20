8934058e7343749cb12e419d042bfbd9d17868c2
Ellan <ellan@Ellan-Gichuru.local>
2024-07-20 16:05:05 +0300

    mac rejects tailiwind

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import Home from "./Home";
+import './output.css';
+
+const App=() => {
+
+
+  return (<Home />)
+   
+}
+export default App
