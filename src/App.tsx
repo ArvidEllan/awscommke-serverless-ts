ba56f5b29f835270a525a40bd076ae7b97d65a7b
Ellan <ellan@Ellan-Gichuru.local>
2024-07-21 17:03:49 +0300

    fix nav components & avatar

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@
 // import Home from "./Home";
 import "./index.css";
 import Test from "./Test";
+import NavbarComponent from "./components/NavbarComponent";
 
 const App = () => {
   return (
     <>
       {/* <Home /> */}
+      <NavbarComponent />
       <Test />
     </>
   );
--- src/Test.tsx
+++ src/Test.tsx
@@ -1,11 +1,52 @@
+import AvatarComponents from "./components/AvatarComponents";
+
 const Test = () => {
   return (
     <div className="border border-red-500 min-h-screen flex flex-col">
+      {/* <nav className="bg-gray-800 p-4">
+        <ul className="flex justify-center space-x-6">
+          <li>
+            <a href="#about" className="text-white hover:text-gray-300">
+              About
+            </a>
+          </li>
+          <li>
+            <a href="#speakers" className="text-white hover:text-gray-300">
+              Speakers
+            </a>
+          </li>
+          <li>
+            <a href="#agenda" className="text-white hover:text-gray-300">
+              Agenda
+            </a>
+          </li>
+          <li>
+            <a href="#volunteers" className="text-white hover:text-gray-300">
+              Volunteers
+            </a>
+          </li>
+          <li>
+            <a href="#sponsors" className="text-white hover:text-gray-300">
+              Sponsors
+            </a>
+          </li>
+          <li>
+            <a href="#venue" className="text-white hover:text-gray-300">
+              Venue
+            </a>
+          </li>
+          <li>
+            <a href="#contact" className="text-white hover:text-gray-300">
+              Contact Us
+            </a>
+          </li>
+        </ul>
+      </nav> */}
       <div className="text-center justify-center border border-white m-8">
         AWS Community Kenya
       </div>
       <div className="flex flex-grow border border-blue-500 justify-center">
-        <div className="w-1/2 p-4 border border-green-500 m-6 max-h-[50vh] overflow-y-auto">
+        <div className="w-1/2 p-4 border border-green-500 m-2 max-h-[80vh] overflow-y-auto">
           AWS Community Day is a dynamic event celebrating the AWS community,
           uniting cloud enthusiasts, developers, and professionals from diverse
           fields. This event highlights the expansive universe of AWS
@@ -17,26 +58,10 @@ const Test = () => {
           designed to provide a platform for learning and sharing, with
           opportunities to gain insights from AWS experts and industry leaders.
           Join us to connect with peers, enhance your AWS skills, and become
-          part of a thriving community driven by shared learning and growth. As
-          GenAI models grow more complex, successfully optimizing their
-          accuracy, cost efficiency, and deployment velocity on AWS becomes both
-          increasingly critical and challenging. This session will provide an
-          end-to-end guide for GenAI teams to maximize their workload efficiency
-          on AWS. We’ll provide an overview of the full GenAI development
-          lifecycle then do a deep dive into progressive model optimization
-          techniques—from prompt engineering to RAG and fine tuning—exploring
-          how each incrementally improves accuracy while minimizing retraining
-          compute requirements. Shifting gears, we’ll share AWS best practices
-          for optimizing GenAI model packaging, deployment, and inference using
-          containers and hardware acceleration. Monitoring and maintaining
-          production GenAI workloads presents its own challenges, which we’ll
-          address through observability, data drift detection, and model
-          degradation monitoring techniques and tools on AWS. Attendees will
-          walk away with a clear framework for incrementally optimizing their
-          GenAI workloads throughout the machine learning lifecycle with an eye
-          toward maximizing performance while keeping costs in check.
+          part of a thriving community driven by shared learning and growth.
         </div>
-        <div className="w-1/2 p-4 border border-blue-500 m-6 max-h-[50vh] overflow-y-auto">
+        <div className="w-1/2 p-4 border border-blue-500 m-2 max-h-[80vh] overflow-y-auto">
+          <AvatarComponents />
           AWS Community Day is a dynamic event celebrating the AWS community,
           uniting cloud enthusiasts, developers, and professionals from diverse
           fields. This event highlights the expansive universe of AWS
