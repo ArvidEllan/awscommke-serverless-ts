9339ae1d45ece3585d26ca1aa116856f6f205e65
TechGirl-254 <wammymuriu@gmail.com>
2024-07-22 22:10:16 +0300

    Creating the navbar, and its components

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,48 @@
-// import Home from "./Home";
-import "./index.css";
-import Test from "./Test";
-import NavbarComponent from "./components/NavbarComponent";
+import React from "react";
+import { ParallaxProvider, ParallaxBanner } from "react-scroll-parallax";
+import Home from "./Home";
+import Text from "./Test";
 
-const App = () => {
+const App: React.FC = () => {
+  const background = {
+    image: "/plain-white-image.png",
+    translateY: [0, 50] as [number, number],
+    opacity: [1, 0.3] as [number, number],
+    scale: [1.05, 1] as [number, number],
+    shouldAlwaysCompleteAnimation: true,
+  };
+
+  const headline = {
+    translateY: [0, 30] as [number, number],
+    scale: [1, 1.05] as [number, number],
+    shouldAlwaysCompleteAnimation: true,
+    expanded: false,
+    children: (
+      <div className="absolute inset-0 flex items-center justify-center">
+        <Home />
+      </div>
+    ),
+  };
+
+  const gradientOverlay = {
+    opacity: [0, 0.9] as [number, number],
+    shouldAlwaysCompleteAnimation: true,
+    expanded: false,
+    children: (
+      <div className="absolute inset-0 bg-gradient-to-t from-gray-900 to-blue-900" />
+    ),
+  };
+
   return (
-    <>
-      {/* <Home /> */}
-      <NavbarComponent />
-      <Test />
-    </>
+    <ParallaxProvider>
+      <ParallaxBanner
+        layers={[background, headline, gradientOverlay]}
+        className="absolute inset-0 h-full w-full"
+      />
+      <section className="relative z-10">
+        <Text />
+      </section>
+    </ParallaxProvider>
   );
 };
 
